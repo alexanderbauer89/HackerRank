f3d9dad63cdd227ce24425916f917073b6c6351f
Alexander Bauer <94492708+alexanderbauer89@users.noreply.github.com>
2021-11-18 23:28:32 +0200

    Update Python If-Else.py
    updated loop to make it easier readable

--- Practice/Python/Introduction/Python If-Else.py
+++ Practice/Python/Introduction/Python If-Else.py
@@ -7,15 +7,15 @@ import re
 import sys
 
 def weird_or_not_weird(n):
-    if n % 2 != 0:
-        return 'Weird'
-    else:
+    if n % 2 == 0:
         if 2 <= n <= 5:
             return 'Not Weird'
         elif 6 <= n <= 20:
             return 'Weird'
         elif n > 20:
             return 'Not Weird'
+    else:
+        return 'Weird'
 
 if __name__ == '__main__':
     n = int(input().strip())

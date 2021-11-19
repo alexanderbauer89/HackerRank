560afca9afcc351b062295fd8ae72d48f4c301d4
Alexander Bauer <94492708+alexanderbauer89@users.noreply.github.com>
2021-11-19 15:44:58 +0200

    Create Alphabet_Rangoli.py

---
+++ Practice/Python/Strings/Alphabet_Rangoli.py
@@ -0,0 +1,12 @@
+import string
+
+def print_rangoli(size):
+    alpha  = string.ascii_lowercase
+    mylist = []
+    for i in range(n):
+        s = "-".join(alpha[i:n])
+        mylist.append((s[::-1] + s[1:]).center(4  * n - 3, "-"))
+    print('\n'.join(mylist[:0:-1] + mylist))
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)

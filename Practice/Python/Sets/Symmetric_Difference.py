ec2bb940616a53fd387dbf803d69434712dd9206
Alexander Bauer <94492708+alexanderbauer89@users.noreply.github.com>
2021-11-20 22:54:33 +0200

    Update Symmetric_Difference.py
    changed raw_input() to input()

--- Practice/Python/Sets/Symmetric_Difference.py
+++ Practice/Python/Sets/Symmetric_Difference.py
@@ -2,6 +2,6 @@ def symmetric_difference(a, b):
     return sorted(set(a ^ b), key=int)
 
 if __name__ == '__main__':
-    _,a = input(), set(raw_input().split())
-    _,b = input(), set(raw_input().split())
+    _,a = input(), set(input().split())
+    _,b = input(), set(input().split())
     print('\n'.join(symmetric_difference(a, b)))
